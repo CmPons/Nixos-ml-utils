09c9068b4af67e11e43c68dcaa5c5bb6128d7094
cmpons <cmpons@gmail.com>
2026-02-26 12:36:37 +0100

    fix: Slimmed down ml-utils

--- flake.nix
+++ flake.nix
@@ -4,21 +4,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     claude-desktop.url = "github:k3d3/claude-desktop-linux-flake";
   };
-  outputs = { self, nixpkgs, claude-desktop }: {
-    homeManagerModules.default = { pkgs, lib, config, ... }:
-      import ./home-module.nix {
-        pkgs = import nixpkgs {
-          system = pkgs.system;
-          config.allowUnfree = true;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      claude-desktop,
+    }:
+    {
+      homeManagerModules.default =
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        import ./home-module.nix {
+          pkgs = import nixpkgs {
+            system = pkgs.system;
+            config.allowUnfree = true;
+          };
+          inherit lib config;
+          claude-desktop = claude-desktop;
         };
-        inherit lib config;
-        claude-desktop = claude-desktop;
-      };
-
-    nixosModules.default = { pkgs, lib, config, ... }:
-      import ./nixos-module.nix {
-        pkgs = nixpkgs.legacyPackages.${pkgs.system};
-        inherit lib config;
-      };
-  };
+    };
 }
